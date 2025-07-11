cb25248f06bfeb7f8cddc97b369620912c21ef83
Tomas <tomasitoxd@outlook.com>
2025-07-11 07:12:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -199,3 +199,25 @@ function eliminarAutoHistorial(diaIndex, autoIndex) {
     renderizar();
   }
 }
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCCFGUUoMiASv_A9zg8oN3FNnvCSUYmZBE",
+  authDomain: "estacionamiento-8e570.firebaseapp.com",
+  projectId: "estacionamiento-8e570",
+  storageBucket: "estacionamiento-8e570.firebasestorage.app",
+  messagingSenderId: "904458707495",
+  appId: "1:904458707495:web:25a01e466622d50f4fe402",
+  measurementId: "G-PGME7T31JF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
